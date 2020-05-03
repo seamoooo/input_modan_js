3af36c1d27f34d3e99829a10c8d8af01246e7629
shumashimo <shm.syuma@i.softbank.jp>
2020-05-03 16:32:15 +0900

    js extends

--- dotinstall/jsObject/main.js
+++ dotinstall/jsObject/main.js
@@ -1,29 +1,6 @@
 'use strict';
 
 {
-class SponsorePost {
-  constructor(text, sponsor) {
-    this.text = text
-    this.likeCount = 0
-  }
-
-  show() {
-    console.log(`${this.text} - ${this.likeCount}likes`)
-    console.log(`...suponserd by ${this.sponsor}`)
-  }
-
-  like(){
-    this.likeCount++;
-    this.show();
-  }
-
-  //静的メゾッド
-  //thisは使えない
-  static showInfo() {
-    console.log('post ckass version 1');
-  }
-}
-
 class Post {
   constructor(text) {
     this.text = text
@@ -45,6 +22,20 @@ class Post {
     console.log('post ckass version 1');
   }
 }
+
+class SponsorePost extends Post{
+  constructor(text, sponsor) {
+    super(text);
+    this.sponsor = sponsor
+  }
+
+  show() {
+    super.show();
+    console.log(`...suponserd by ${this.sponsor}`)
+  }
+}
+
+
 
 const posts = [
   new Post('LOVE JS'),
