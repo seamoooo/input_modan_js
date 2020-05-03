39bd0de95b0837bb82ebeb49d151dff8e07f221f
shumashimo <shm.syuma@i.softbank.jp>
2020-05-03 14:39:24 +0900

    class lesson

--- dotinstall/jsObject/main.js
+++ dotinstall/jsObject/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
+{
+class SponsorePost {
+  constructor(text, sponsor) {
+    this.text = text
+    this.likeCount = 0
+  }
+
+  show() {
+    console.log(`${this.text} - ${this.likeCount}likes`)
+    console.log(`...suponserd by ${this.sponsor}`)
+  }
+
+  like(){
+    this.likeCount++;
+    this.show();
+  }
+
+  //静的メゾッド
+  //thisは使えない
+  static showInfo() {
+    console.log('post ckass version 1');
+  }
+}
+
 class Post {
   constructor(text) {
     this.text = text
@@ -7,14 +31,31 @@ class Post {
   }
 
   show() {
-    console.log(`$(this.text) - $(this.likeCount)いいね`)
+    console.log(`${this.text} - ${this.likeCount}likes`)
+  }
+
+  like(){
+    this.likeCount++;
+    this.show();
+  }
+
+  //静的メゾッド
+  //thisは使えない
+  static showInfo() {
+    console.log('post ckass version 1');
   }
 }
 
 const posts = [
   new Post('LOVE JS'),
   new Post('追い込め！'),
+  new SponsorePost('hige','hoge')
 ]
 
-posts[0].show();
+//プロパティを直接操作せずに、クラスにメソッドを追加する
+//カプセル化
+posts[0].like();
 posts[1].show();
+Post.showInfo();
+posts[2].show();
+}
