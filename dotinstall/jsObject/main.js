5775db5058697e4616ba4dcb282897136048ffe4
shumashimo <shm.syuma@i.softbank.jp>
2020-05-05 07:44:39 +0900

    js dom

--- dotinstall/jsObject/main.js
+++ dotinstall/jsObject/main.js
@@ -1,52 +1,15 @@
 'use strict';
 
 {
-class Post {
-  constructor(text) {
-    this.text = text
-    this.likeCount = 0
+  function update() {
+    // document.getElementById('target').textContent = 'Changed!';
+    // document.querySelector('p').textContent = 'Changed!';
+    //何番目か指定する。
+    document.querySelectorAll('p')[1].textContent = 'Changed!';
+    document.querySelectorAll('p').forEach((p,index) => {
+      p.textContent = `${index}番目のpです。`;
+    });
   }
 
-  show() {
-    console.log(`${this.text} - ${this.likeCount}likes`)
-  }
-
-  like(){
-    this.likeCount++;
-    this.show();
-  }
-
-  //静的メゾッド
-  //thisは使えない
-  static showInfo() {
-    console.log('post ckass version 1');
-  }
-}
-
-class SponsorePost extends Post{
-  constructor(text, sponsor) {
-    super(text);
-    this.sponsor = sponsor
-  }
-
-  show() {
-    super.show();
-    console.log(`...suponserd by ${this.sponsor}`)
-  }
-}
-
-
-
-const posts = [
-  new Post('LOVE JS'),
-  new Post('追い込め！'),
-  new SponsorePost('hige','hoge')
-]
-
-//プロパティを直接操作せずに、クラスにメソッドを追加する
-//カプセル化
-posts[0].like();
-posts[1].show();
-Post.showInfo();
-posts[2].show();
+  setTimeout(update, 1000);
 }
