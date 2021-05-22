7f1d654a2e978cef82a49cc1a981faccdef1e21f
志茂秀真 <hm.seamo@gmail.com>
2021-05-23 00:00:35 +0900

    todoアプリ

---
+++ vue/todo/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: [],
+  },
+  methods: {
+    addItem: function (event) {
+      if (this.newItem == '') return;
+      const todo = {
+        item: this.newItem,
+        isDone: false,
+      };
+      this.todos.push(todo);
+      this.newItem = '';
+    },
+    deleteItem: function (index) {
+      // alert(index);
+      this.todos.splice(index, 1);
+    },
+  },
+});
