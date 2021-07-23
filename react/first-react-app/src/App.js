d754fcd75c1f7641b2a45879382fdb66f2538aaf
志茂秀真 <hm.seamo@gmail.com>
2021-07-24 00:46:00 +0900

    関数component

--- react/first-react-app/src/App.js
+++ react/first-react-app/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = 'HI';
-    // キャメルで記述する。
-    const dom = <h1 className="foo">helloworld{greeting}</h1>;
-    // JSXML 内部的にbabelがJSトランスパイルする
-    return (
-      // タグを一つにまとめる必要がある
-      // 認識されたくない場合は、<React.Fragment>使用する
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onChange={() => {
-            console.log('I am checked');
-          }}
-        ></input>
-      </React.Fragment>
-    );
-  }
-}
+// クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     const greeting = 'HI';
+//     // キャメルで記述する。
+//     const dom = <h1 className="foo">helloworld{greeting}</h1>;
+//     // JSXML 内部的にbabelがJSトランスパイルする
+//     return (
+//       // タグを一つにまとめる必要がある
+//       // 認識されたくない場合は、<React.Fragment>使用する
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input
+//           type="text"
+//           onChange={() => {
+//             console.log('I am checked');
+//           }}
+//         ></input>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>しゃーーー</div>;
+};
 
 export default App;
