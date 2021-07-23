c6229d9f80010b0a856de554b2a1f3a395ddc88c
志茂秀真 <hm.seamo@gmail.com>
2021-07-24 00:16:21 +0900

    input jsx

--- react/first-react-app/src/App.js
+++ react/first-react-app/src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const greeting = 'HI';
+    // キャメルで記述する。
+    const dom = <h1 className="foo">helloworld{greeting}</h1>;
+    // JSXML 内部的にbabelがJSトランスパイルする
+    return (
+      // タグを一つにまとめる必要がある
+      // 認識されたくない場合は、<React.Fragment>使用する
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log('I am checked');
+          }}
+        ></input>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
